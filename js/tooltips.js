f88cb5fe6d2e5a1aa541aabec263ea2a7607a2dc
Wyn-07 <jbacolod.a12137227@umak.edu.ph>
2024-09-19 10:58:14 +0800

    Delete js directory

--- js/tooltips.js
+++
@@ -1,9 +0,0 @@
-function showTooltip(element) {
-    var tooltip = element.querySelector('.tooltiptexts');
-    tooltip.style.visibility = 'visible';
-}
-
-function hideTooltip(element) {
-    var tooltip = element.querySelector('.tooltiptexts');
-    tooltip.style.visibility = 'hidden';
-}
